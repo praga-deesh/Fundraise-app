c5e5fc36b51f8acea347e75da6b4a5c2c33f6722
Ashmithaa-23 <aannadu1@ford.com>
2024-02-21 11:28:44 +0530

    commentd

--- src/main/java/org/company/fundraisedemo/donar/DonorController.java
+++ src/main/java/org/company/fundraisedemo/donar/DonorController.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class DonorController {
     @Autowired
     private DonorService donorService;
-//hellohi
+//ashhhhh
     @PostMapping("donor/createNewProfile")
     public Donor createDonor(@RequestBody Donor newDonor) throws DonorExceptions {
         return this.donorService.createDonorProfile(newDonor);
