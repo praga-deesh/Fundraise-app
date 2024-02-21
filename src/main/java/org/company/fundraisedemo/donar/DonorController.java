44aff0d5a9765757368b91cc59872aa8232588a4
K J <pragadeeshclone@gmail.com>
2024-02-21 11:18:45 +0530

    abc

--- src/main/java/org/company/fundraisedemo/donar/DonorController.java
+++ src/main/java/org/company/fundraisedemo/donar/DonorController.java
@@ -13,7 +13,5 @@ public class DonorController {
     @PostMapping("donors")
     public Donor createDonor(@RequestBody Donor newDonor) {
         return this.donorService.createDonor(newDonor);
-
-        // abc
     }
 }
