c136b26af8c68003db08650e46e08ca5deef0483
kishore010602 <kishore010602@gmail.com>
2024-02-27 08:27:03 +0530

    testCasesAdded

--- src/test/java/org/company/fundraisedemo/post/PostServiceImplTest.java
+++ src/test/java/org/company/fundraisedemo/post/PostServiceImplTest.java
@@ -45,6 +45,54 @@ class PostServiceImplTest {
         Assertions.assertNotNull(postRepositoryDao.findById(post.getId()));
 
     }
+    @Test
+    void updatePostTest() throws PostExceptions {
+
+        Fundraiser fundraiser = new Fundraiser("john","dane@gmail.com","dami");
+        try {
+            fundraiserService.createFundraiserProfile(fundraiser);
+        } catch (FundraiserExceptions e) {
+            throw new RuntimeException(e);
+        }
+        Post post = new Post("Poverty", "Expenses for food and shelter for poors", "poverty", LocalDate.of(2024, 02, 27), LocalDate.of(2024, 04, 20), 200000.0, 0.0, "incomplete", fundraiser);
+        postService.addNewPost(post);
+
+        post.setTitle("Updated Title");
+        post.setDescription("Updated Description");
+        post.setAmountRequested(300000.0);
+        try {
+            Post updatedPost = postService.updatePost(post);
+            assertNotNull(updatedPost);
+            assertEquals("Updated Title", updatedPost.getTitle());
+            assertEquals("Updated Description", updatedPost.getDescription());
+            assertEquals(300000.0, updatedPost.getAmountRequested());
+        } catch (PostExceptions e) {
+            fail("Exception thrown while updating post: " + e.getMessage());
+        }
+    }
+    @Test
+    void deletePostByIdTest() throws PostExceptions {
+        Fundraiser fundraiser = new Fundraiser("johny","dame@gmail.com","dames");
+        try {
+            fundraiserService.createFundraiserProfile(fundraiser);
+        } catch (FundraiserExceptions e) {
+            throw new RuntimeException(e);
+        }
+        Post post = new Post("Poverty", "Expenses for food and shelter for poors", "poverty", LocalDate.of(2024, 02, 27), LocalDate.of(2024, 04, 20), 200000.0, 0.0, "incomplete", fundraiser);
+        postService.addNewPost(post);
+
+        Integer postId = post.getId();
+        try {
+            Post deletedPost = postService.deletePostById(postId);
+            assertNotNull(deletedPost);
+            assertEquals(postId, deletedPost.getId());
+        } catch (PostExceptions e) {
+            fail("Exception thrown while deleting post by ID: " + e.getMessage());
+        }
+
+        assertFalse(postRepositoryDao.findById(postId).isPresent());
+    }
+
 
 
 }
